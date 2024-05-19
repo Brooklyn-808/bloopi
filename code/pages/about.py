7cf1338b822ebbb971d80231b37209f03c6d80c9
Brooklyn 808 <142480789+Brooklyn-808@users.noreply.github.com>
2024-05-19 18:58:45 +0800

    Update about.py

--- code/pages/about.py
+++ code/pages/about.py
@@ -1,1 +1,4 @@
-
+import streamlit as st
+st.title("About Me")
+st.write("I am a passionate developer who loves creating awesome projects.")
+st.write("In my free time, I enjoy learning new technologies and building cool stuff.")
